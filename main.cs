47d7846f82e4132e215751c9183cb087cc77c1d5
cullamco2130713 <31803560-cullamco2130713@users.noreply.replit.com>
2024-02-03 02:52:06 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Miguel"; 
+    string lastName = "Cullamco";
+    int age = 18;
+    Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
+    Console.WriteLine ("Hello World " + firstName + " "+ lastName"+" "+ age);
+}
+  }  
+  
